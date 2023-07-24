ea6376f0f36c87f350e25656d860ad0a17a55950
SUMIT SHANKAR <shankarsumit33@gmail.com>
2023-07-24 22:08:10 +0530

    testing

---
+++ components/greet/Greet.tsx
@@ -0,0 +1,9 @@
+import { GreetProps } from "./Greet-types"
+export const Greet = (props: GreetProps) => {
+  return (
+    <div>Hello {props.name? props.name: "Guest"}</div>
+  )
+}
+
+
+
---
+++ hooks/use-counter/useCounter.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { useState } from 'react'
+import { UseCounterProps } from './userCouner.types'
+
+export const useCounter = ({ initialCount = 0 }: UseCounterProps = {}) => {
+  const [count, setCount] = useState(initialCount)
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+  return { count, increment, decrement }
+}
